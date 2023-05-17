2d10988367c710fcea926f526bf70c1c9c26bb60
Stephen Sanwo <Stephen.sanwo@icloud.com>
2023-05-17 04:59:43 +0100

    feat: add basic header

--- react-auth/src/pages/Layout.tsx
+++ react-auth/src/pages/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Theme } from "@carbon/react";
+import Header from "../components/Header";
 
 const Layout = () => {
   return (
     <Theme theme="g100">
+      <Header />
       <Outlet />
     </Theme>
   );
